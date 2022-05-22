589bd9890d70a811d997722e449fd7071b73f3db
TNTerminator <tnterminator1@gmail.com>
2022-05-22 20:48:31 +0200

    close #1

---
+++ src/main/java/it/unipd/mtss/App.java
@@ -0,0 +1,18 @@
+////////////////////////////////////////////////////////////////////
+// [FEDERICO] [DE SANCTIS] [2009107]
+// [GABRIELE] [SARACCO] [2009997]
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
